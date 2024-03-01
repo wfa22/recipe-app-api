fcc93d5d447007bcedf15791a4ea447f5fe2f8e3
Alexey Bodanyuk <144894236+wfa22@users.noreply.github.com>
2024-03-01 17:59:18 +0300

    Added tests for user API

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
